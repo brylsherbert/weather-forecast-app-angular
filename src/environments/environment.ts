e73afdd09fee358d95ba6f4220822bf1abb98584
Bryl Magdadaro <magdadarobryl@gmail.com>
2026-01-20 09:33:47 +0800

    chore: update env placeholder

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
   apiUrl: 'https://api.weatherapi.com',
-  weatherApiKey: 'YOUR_API_KEY_HERE', // Get your free API key from https://www.weatherapi.com
+  weatherApiKey: '__WEATHER_API_KEY__', // Get your free API key from https://www.weatherapi.com
   cacheTimeout: 300000 // 5 minutes in milliseconds
 };
