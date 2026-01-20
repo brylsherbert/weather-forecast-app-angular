d83e0fa48ddfc89d0e6527dbe06c2a99d1fa6f17
Bryl Magdadaro <magdadarobryl@gmail.com>
2026-01-20 08:50:14 +0800

    chore: add environment files with placeholders for Netlify deployment

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://api.weatherapi.com',
+  weatherApiKey: 'YOUR_API_KEY_HERE', // Get your free API key from https://www.weatherapi.com (replaced during Netlify build)
+  cacheTimeout: 300000 // 5 minutes in milliseconds
+};
